b0ce4d8dac662aa4a674303c3dbb1be29b192b6e
adivarshnei <avavav.adersh.varshnei@gmail.com>
2023-05-28 05:20:52 +0530

    Basic Site

--- main-site.py
+++ main-site.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+from matplotlib import pyplot as plt
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello() -> str:
-    return "Hello World!"
+    return render_template("home.html")
+
+
+@app.route("/about")
+def about() -> str:
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
